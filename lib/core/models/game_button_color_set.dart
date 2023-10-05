61d94c64d17bd4494ccbe54281af15a328013d5a
mikeamir <mikeamiris@gmail.com>
2023-10-05 14:35:21 +0300

    Updated game button and added more color sets

--- lib/core/models/game_button_color_set.dart
+++ lib/core/models/game_button_color_set.dart
@@ -19,4 +19,32 @@ class GameButtonColorSet {
     shadow: Color(0xff4EC307),
     border: Colors.white,
   );
+
+  static const GameButtonColorSet yellow = GameButtonColorSet(
+    primary: Color(0x80FFEE88),
+    secondary: Color(0xffFFDB0A),
+    shadow: Color(0xffFFB213),
+    border: Colors.white,
+  );
+
+  static const GameButtonColorSet pink = GameButtonColorSet(
+    primary: Color(0x80FDC0FF),
+    secondary: Color(0xffFC8AFF),
+    shadow: Color(0xffDA57F0),
+    border: Colors.white,
+  );
+
+  static const GameButtonColorSet cyan = GameButtonColorSet(
+    primary: Color(0x8094E7FC),
+    secondary: Color(0xff4CDAFE),
+    shadow: Color(0xff08B9FF),
+    border: Colors.white,
+  );
+
+  static const GameButtonColorSet purple = GameButtonColorSet(
+    primary: Color(0x80D6AFFE),
+    secondary: Color(0xffC286FF),
+    shadow: Color(0xffA75CF4),
+    border: Colors.white,
+  );
 }
--- lib/widgets/game_button.dart
+++ lib/widgets/game_button.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 import '../core/models/game_button_color_set.dart';
 
+enum BorderType { rounded, circular }
+
 class GameButton extends StatelessWidget {
   final String text;
   final VoidCallback onTap;
   final double horizontalPadding;
   final double height;
   final GameButtonColorSet colorSet;
+  final BorderType borderType;
 
   const GameButton({
     required this.text,
@@ -14,18 +17,21 @@ class GameButton extends StatelessWidget {
     this.colorSet = GameButtonColorSet.green,
     this.horizontalPadding = 24,
     this.height = 48,
+    this.borderType = BorderType.circular,
     super.key,
   });
 
   @override
   Widget build(BuildContext context) {
+    final bool isRoundedBorder = borderType == BorderType.rounded;
+    final double shadowBorderRadius = isRoundedBorder ? 8 : 33;
     return GestureDetector(
       onTap: onTap,
       child: Container(
         height: height,
         decoration: BoxDecoration(
           color: colorSet.shadow,
-          borderRadius: BorderRadius.circular(22),
+          borderRadius: BorderRadius.circular(isRoundedBorder ? 8 : 22),
           border: Border.all(color: Colors.white, width: 3),
         ),
         child: Column(
@@ -41,9 +47,9 @@ class GameButton extends StatelessWidget {
                           child: Container(
                             decoration: BoxDecoration(
                               color: colorSet.primary,
-                              borderRadius: const BorderRadius.only(
-                                topLeft: Radius.circular(33),
-                                bottomLeft: Radius.circular(33),
+                              borderRadius: BorderRadius.only(
+                                topLeft: Radius.circular(shadowBorderRadius),
+                                bottomLeft: Radius.circular(shadowBorderRadius),
                               ),
                             ),
                           ),
@@ -52,9 +58,9 @@ class GameButton extends StatelessWidget {
                           child: Container(
                             decoration: BoxDecoration(
                               color: colorSet.secondary,
-                              borderRadius: const BorderRadius.only(
-                                topRight: Radius.circular(33),
-                                bottomRight: Radius.circular(33),
+                              borderRadius: BorderRadius.only(
+                                topRight: Radius.circular(shadowBorderRadius),
+                                bottomRight: Radius.circular(shadowBorderRadius),
                               ),
                             ),
                           ),
