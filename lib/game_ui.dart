bcc8b55202600d10b4dff2cca0b08608b6fb9b58
mikeamir <mikeamiris@gmail.com>
2023-10-07 02:11:46 +0300

    Added Game Card

--- lib/game_ui.dart
+++ lib/game_ui.dart
@@ -2,6 +2,7 @@ export './widgets/game_button.dart';
 export './widgets/game_icon_button.dart';
 export './widgets/game_progress_bar.dart';
 export './widgets/mini_game_button.dart';
+export './widgets/game_card.dart';
 export './core/models/game_button_color_set.dart';
 export './core/models/game_icon_button_color_set.dart';
 export './core/models/game_progress_bar_color_set.dart';
---
+++ lib/widgets/game_card.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class GameCard extends StatelessWidget {
+  final double width;
+  final double height;
+  final Color color;
+  final Color borderColor;
+  final double borderThickness;
+  final VoidCallback? onTap;
+  final Alignment alignment;
+  final double borderRadius;
+  final List<BoxShadow> shadows;
+  final Widget child;
+
+  const GameCard({
+    required this.width,
+    required this.height,
+    required this.child,
+    this.onTap,
+    this.borderColor = const Color(0xffB57BFE),
+    this.color = Colors.white,
+    this.alignment = Alignment.center,
+    this.borderRadius = 16,
+    this.borderThickness = 3,
+    this.shadows = const [
+      BoxShadow(
+        color: Color(0x26000000),
+        offset: Offset(0, 6),
+      ),
+      BoxShadow(
+        color: Color.fromARGB(255, 89, 24, 240),
+        offset: Offset(0, 3),
+      ),
+    ],
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: height,
+      width: width,
+      alignment: alignment,
+      decoration: BoxDecoration(
+        color: color,
+        borderRadius: BorderRadius.circular(borderRadius),
+        border: Border.all(color: borderColor, width: borderThickness),
+        boxShadow: shadows,
+      ),
+      child: child,
+    );
+  }
+}
